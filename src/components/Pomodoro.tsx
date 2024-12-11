5f87d33858fa6ad7df8aa7073d87566ff08429c8
PNS212624\Hansol <yrlee00@hansol.com>
2024-12-11 13:11:17 +0900

    [feat] motion 아이콘으로 변경 및 날씨 id에 따라 배경 변경 로직 구현

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { AnimatePresence, motion } from "framer-motion";
 import { useRecoilState } from "recoil";
 import { clickAtom, timerAtom } from "../services/Atom.ts";
+import {PlayBtn} from "./PlayBtn.tsx";
+import {PauseBtn} from "./PauseBtn.tsx";
 
 const BtnVar = {
     hover: { scale: 1.3 }
@@ -22,6 +24,7 @@ const BoxVar = {
 
 interface PomodoroProps {
     weather: number;
+    weatherId: number;
 }
 
 function Pomodoro({ weather }: PomodoroProps) {
@@ -54,14 +57,97 @@ function Pomodoro({ weather }: PomodoroProps) {
         }
     }, [timer, clicked]);
 
+
+    const bgColor = (weather:number) => {
+        switch (weather) {
+            case 200:
+            case 201:
+            case 202:
+            case 210:
+            case 211:
+            case 212:
+            case 221:
+            case 230:
+            case 231:
+            case 232:
+                return 'bg-yellow-300'; // 200번대: 천둥구름 관련
+                break;
+            case 300:
+            case 301:
+            case 302:
+            case 310:
+            case 311:
+            case 312:
+            case 313:
+            case 314:
+            case 321:
+                return 'bg-green-300'; // 300번대: 안개비 및 소나기 관련
+            case 500:
+            case 501:
+            case 502:
+            case 503:
+            case 504:
+                return 'bg-blue-500'; // 500번대: 비 관련
+            case 600:
+            case 601:
+            case 602:
+            case 611:
+            case 612:
+            case 615:
+            case 616:
+            case 620:
+            case 621:
+            case 622:
+                return 'bg-slate-200'; // 600번대: 눈 및 진눈깨비 관련
+            case 701:
+            case 711:
+            case 721:
+            case 731:
+            case 741:
+            case 751:
+            case 761:
+            case 762:
+            case 771:
+                return 'bg-amber-600'; // 700번대: 안개 및 먼지 관련
+            case 800:
+            case 801:
+            case 802:
+            case 803:
+            case 804:
+                return 'bg-cyan-300'; // 800번대: 맑은 하늘 및 구름 관련
+            case 900:
+            case 901:
+            case 902:
+            case 903:
+            case 904:
+            case 905:
+            case 906:
+            case 951:
+            case 952:
+            case 953:
+            case 954:
+            case 955:
+            case 956:
+            case 957:
+            case 958:
+            case 959:
+            case 960:
+            case 961:
+            case 962:
+                return 'bg-rose-300'; // 900번대: 폭풍 및 허리케인 관련
+            default:
+                return  'bg-gray-200'; // 기본 색상
+        }
+    }
+
     return (
-        <div className="bg-teal-400">
+        <div className={`${bgColor(weather)}`}>
             <div className="flex flex-col items-center justify-center  w-[480px] h-screen mx-auto">
-                <h1 className="text-white text-4xl">Pomodoro</h1>
+                <h1 className="text-white text-4xl font-bold">Pomodoro</h1>
                 <AnimatePresence>
                     <div className="flex flex-row mt-24">
                         <motion.div
-                            className="w-[120px] h-[200px] bg-white shadow-lg rounded-lg text-teal-400 flex items-center justify-center text-5xl font-bold"
+                            className="w-[120px] h-[200px] bg-white shadow-lg rounded-lg text-black flex items-center justify-center text-5xl font-bold"
                             variants={BoxVar}
                             initial="start"
                             animate="end"
@@ -74,7 +160,7 @@ function Pomodoro({ weather }: PomodoroProps) {
                             <div className="bg-white opacity-50 w-4 h-4 rounded-full m-2"></div>
                         </div>
                         <motion.div
-                            className="w-[120px] h-[200px] bg-white shadow-lg rounded-lg text-teal-400 flex items-center justify-center text-5xl font-bold"
+                            className="w-[120px] h-[200px] bg-white shadow-lg rounded-lg text-black flex items-center justify-center text-5xl font-bold"
                             variants={BoxVar}
                             initial="start"
                             animate="end"
@@ -85,12 +171,12 @@ function Pomodoro({ weather }: PomodoroProps) {
                     </div>
                     <motion.button
                         onClick={onClick}
-                        className="mt-24 w-[100px] h-[100px] bg-black opacity-30 rounded-full text-white text-5xl font-bold"
+                        className="mt-24 w-[80px] h-[80px] bg-black opacity-30 rounded-full text-white text-5xl font-bold"
                         variants={BtnVar}
                         whileHover="hover"
                         key={clicked.toString()}
                     >
-                        {clicked ? "||" : "▶"}
+                        {clicked ? <PauseBtn /> : <PlayBtn />}
                     </motion.button>
                 </AnimatePresence>
                 <div className="flex flex-row mt-20">
