ce44305f1e67ea1392a2b70911fd4fb7d0647ea2
PNS212624\Hansol <yrlee00@hansol.com>
2024-12-13 10:03:40 +0900

    [refactor] custom hook으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
-import { getLoactionWeather } from "./services/api.ts";
 import Pomodoro from "./components/Pomodoro.tsx";
+import useWeather from "./utils/hooks.tsx";
 
 function App() {
-  const [weather, setWeather] = useState<any>(null);
-
-  useEffect(() => {
-    getLoactionWeather()
-      .then((data) => {
-        setWeather(data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+  const weather = useWeather();
 
   return (
     <>
---
+++ src/utils/hooks.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { getLoactionWeather } from "../services/api.ts";
+
+export default function useWeather() {
+  const [weather, setWeather] = useState<any>(null);
+
+  useEffect(() => {
+    getLoactionWeather()
+      .then((data) => {
+        setWeather(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return weather;
+}
