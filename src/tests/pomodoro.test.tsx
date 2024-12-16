dc29f3101b2cb7dc0e81fbed6504fea2a0b093db
Leeyoungryul <lekjkj@naver.com>
2024-12-16 22:00:01 +0900

    [feat] 테스트 코드 작성

--- src/tests/pomodoro.test.tsx
+++ src/tests/pomodoro.test.tsx
@@ -18,12 +18,123 @@ describe("pomodoro", () => {
     });
 
     it("날씨 API에서 200번대 데이터가 들어온다.", () => {
+        const bgColor = "bg-yellow-300"
+        const icon = "⚡️"
+
         render(
             <RecoilRoot>
-                <Pomodoro weather={200} />
+                <Pomodoro weather={200}/>
             </RecoilRoot>
         );
 
+        const container = screen.getByText(/Pomodoro/i).closest('div').parentElement;
+        expect(container).toHaveClass(bgColor);
+
+        const heading = screen.getByText(/Pomodoro/i);
+        expect(heading).toHaveTextContent(icon);
+    })
+
+    it("날씨 API에서 300번대 데이터가 들어온다.", () => {
+        const bgColor = "bg-green-300"
+        const icon = "🌧️"
+
+        render(
+            <RecoilRoot>
+                <Pomodoro weather={300}/>
+            </RecoilRoot>
+        );
+
+        const container = screen.getByText(/Pomodoro/i).closest('div').parentElement;
+        expect(container).toHaveClass(bgColor);
+
+        const heading = screen.getByText(/Pomodoro/i);
+        expect(heading).toHaveTextContent(icon);
+    })
+
+    it("날씨 API에서 500번대 데이터가 들어온다.", () => {
+        const bgColor = "bg-blue-500"
+        const icon = "☔️"
+
+        render(
+            <RecoilRoot>
+                <Pomodoro weather={500}/>
+            </RecoilRoot>
+        );
+
+        const container = screen.getByText(/Pomodoro/i).closest('div').parentElement;
+        expect(container).toHaveClass(bgColor);
+
+        const heading = screen.getByText(/Pomodoro/i);
+        expect(heading).toHaveTextContent(icon);
+
+    })
+
+    it("날씨 API에서 600번대 데이터가 들어온다.", () => {
+        const bgColor = "bg-slate-200"
+        const icon = "❄️"
+
+        render(
+            <RecoilRoot>
+                <Pomodoro weather={600}/>
+            </RecoilRoot>
+        );
+
+        const container = screen.getByText(/Pomodoro/i).closest('div').parentElement;
+        expect(container).toHaveClass(bgColor);
+
+        const heading = screen.getByText(/Pomodoro/i);
+        expect(heading).toHaveTextContent(icon);
+    })
+
+    it("날씨 API에서 700번대 데이터가 들어온다.", () => {
+        const bgColor = "bg-amber-600"
+        const icon = "🌫️"
+
+        render(
+            <RecoilRoot>
+                <Pomodoro weather={701}/>
+            </RecoilRoot>
+        );
+
+        const container = screen.getByText(/Pomodoro/i).closest('div').parentElement;
+        expect(container).toHaveClass(bgColor);
+
+        const heading = screen.getByText(/Pomodoro/i);
+        expect(heading).toHaveTextContent(icon);
+    })
+
+    it("날씨 API에서 800번대 데이터가 들어온다.", () => {
+        const bgColor = "bg-cyan-300"
+        const icon = "☀️"
+
+        render(
+            <RecoilRoot>
+                <Pomodoro weather={800}/>
+            </RecoilRoot>
+        );
+
+        const container = screen.getByText(/Pomodoro/i).closest('div').parentElement;
+        expect(container).toHaveClass(bgColor);
+
+        const heading = screen.getByText(/Pomodoro/i);
+        expect(heading).toHaveTextContent(icon);
+    })
+
+    it("날씨 API에서 900번대 데이터가 들어온다.", () => {
+        const bgColor = "bg-rose-300"
+        const icon = "🌪️"
+
+        render(
+            <RecoilRoot>
+                <Pomodoro weather={900}/>
+            </RecoilRoot>
+        );
+
+        const container = screen.getByText(/Pomodoro/i).closest('div').parentElement;
+        expect(container).toHaveClass(bgColor);
+
+        const heading = screen.getByText(/Pomodoro/i);
+        expect(heading).toHaveTextContent(icon);
     })
 })
 
