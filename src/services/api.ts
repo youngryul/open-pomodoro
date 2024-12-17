09810654a4fb985a39094ff0a5d76522a435a8dc
PNS212624\Hansol <yrlee00@hansol.com>
2024-12-17 11:18:18 +0900

    [fix] env undefined 오류 해결

--- src/services/api.ts
+++ src/services/api.ts
@@ -17,7 +17,7 @@ export function getLoactionWeather(): Promise<number> {
 }
 
 async function getWeather(lat: number, lon: number) {
-  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}`;
+    const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}`;
 
   const result =  await axios.get(WEATHER_API);
   return result.data.weather[0].id;
